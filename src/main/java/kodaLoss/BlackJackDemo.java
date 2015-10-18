f95c694f47f2d376028f316e632e59eed8455ad7
tetrominos <contact@johansoderblom.se>
2015-10-18 12:42:15 +0200

    ta bort tester ur main

--- src/main/java/kodaLoss/BlackJackDemo.java
+++ src/main/java/kodaLoss/BlackJackDemo.java
@@ -10,12 +10,6 @@ public class BlackJackDemo {
 		
 		// Main guiMain = new Main();
 		// Använda Gui.Main klassen som Demo direkt?
-		CardShoe cs = new CardShoe();
-		ArrayList<Card> theCs= cs.getCardShoe();
-		System.out.println(theCs);
-		boolean test = theCs.size() == 208;
-		System.out.println(theCs.size());
-		System.out.println(test);
 	}
 	
 	
