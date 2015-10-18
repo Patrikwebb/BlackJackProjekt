30e45713b992f04fc8a5e10bb45d1ad85926caf8
Langhaus <timlanghans34@gmail.com>
2015-10-18 18:18:50 +0200

    fixed problem, TIM?!

--- src/main/java/kodaLoss/BlackJackDemo.java
+++ src/main/java/kodaLoss/BlackJackDemo.java
@@ -2,14 +2,19 @@ package kodaLoss;
 
 import java.util.ArrayList;
 
+import gui.Main;
+
 // import gui.Main;
 
 public class BlackJackDemo {
 
 	public static void main(String[] args) {
 		
-		// Main guiMain = new Main();
-		// Använda Gui.Main klassen som Demo direkt?
+	  
+	  
+		//Main guiMain = new Main(bank);
+		
+		Main.launch(Main.class , args);
 	}
 	
 	
