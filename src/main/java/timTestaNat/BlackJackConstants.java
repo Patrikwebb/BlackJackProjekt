4086bc4b24985479165a820a64772c84842d5dcb
PatrikWebb <Patrikwebb@hotmail.com>
2015-10-19 23:03:30 +0200

    Extra mellanslag ;)

--- src/main/java/timTestaNat/BlackJackConstants.java
+++ src/main/java/timTestaNat/BlackJackConstants.java
@@ -6,4 +6,5 @@ public class BlackJackConstants {
   public final static String FILE_PICTURE_PROTOKOLL = "file://";
   public final static String FILE_PICTURE_PATH = "src/CardPictures/";
   public final static String FILE_PICTURE_FILEENDING = ".png";
+  
 }
